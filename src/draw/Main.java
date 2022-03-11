23112db773a35fb672ea1b15c64bf1f827e863d8
kevin stamenkovski <kevin.stamenkovski@gmail.com>
2022-03-11 20:42:44 +0100

    created file

---
+++ src/draw/Main.java
@@ -0,0 +1,13 @@
+package draw;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("         JJ               AA           VV                  VV           AA                      ");
+        System.out.println("         JJ             AA  AA           VV              VV           AA  AA                    ");
+        System.out.println("         JJ           AA      AA           VV          VV           AA      AA                  ");
+        System.out.println("         JJ         AAAAAAAAAAAAAA           VV      VV           AAAAAAAAAAAAAA                ");
+        System.out.println("JJ       JJ       AA              AA           VV  VV           AA              AA              ");
+        System.out.println("  JJJJJJ        AA                  AA           VV           AA                  AA            ");
+    }
+}
